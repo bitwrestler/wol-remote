f0e47f6ee8854d4857a6bd5a51a34a25e953f993
eric <eric@spenc.org>
2026-06-09 20:04:49 -0400

    endpoint for wake

--- wol-remote.py
+++ wol-remote.py
@@ -1,10 +1,25 @@
-from wollib import wake
 import os
+import wollib
 from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
 
 load_dotenv()
+app = FastAPI()
 
-if __name__ == "__main__":
+def _handle():
     mac_list = [mac.strip() for mac in os.getenv("MAC_ADDRESSES").split(",") if mac.strip()]
     for mac in mac_list:
-        wake(mac)
+        wollib.wake(mac)
+
+@app.get("/api/wake")
+async def wake_endpoint():
+    try:
+        _handle()
+        return {"status": "sent" }
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+
+
+if __name__ == "__main__":
+    _handle()
